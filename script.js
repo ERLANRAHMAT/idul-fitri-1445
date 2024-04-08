78f07f852252c5172fe6ef5cbf14a9d5a56a788f
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-04-09 03:14:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ Iya.addEventListener("click", function () {
         Image.setAttribute("src", "https://c.tenor.com/57JSPYRo0k8AAAAj/lebaran-selamat-lebaran.gif");
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Selamat Hari Raya Idul Fitri 1444 Hijriah";
+        Text.innerHTML = "Selamat Hari Raya Idul Fitri 1445 Hijriah";
         Text.classList.add('text-animate');
         Iya.remove();
     }
